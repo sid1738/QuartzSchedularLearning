92dcb09639e148324457241eaba5600333023575
sid1738 <siddhart.m1738@gmail.com>
2025-08-20 07:45:50 +0530

    quartz

---
+++ src/main/java/com/example/quartz/QuartzLearning/QuartzLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.quartz.QuartzLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzLearningApplication.class, args);
+	}
+
+}
